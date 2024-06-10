62e77e3e280dcfa491276eed5fef89c8e59fc1e9
nrathifa <athifaelysha58@gmail.com>
2024-06-10 23:58:49 +0800

    mainpage, layout, web

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
+    Route::get('/mainpage', function () {
+        return view('mainpage');
     })->name('dashboard');
 });

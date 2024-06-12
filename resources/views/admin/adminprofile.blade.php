5804488e48b2e033f9d99287564f0125d16e8eab
Adlina Rosli <adlinarosli24@gmail.com>
2024-06-12 23:11:21 +0800

    change bg img

--- resources/views/admin/adminprofile.blade.php
+++ resources/views/admin/adminprofile.blade.php
@@ -1,5 +1,19 @@
 @extends('master.layout')
 
+<div id="hero" class="hero route bg-image" style="background-image: url(assets/img/bookshelf.jpg)">
+    <div class="overlay-itro"></div>
+    <div class="hero-content display-table">
+      <div class="table-cell">
+        <div class="container">
+          <!--<p class="display-6 color-d">Hello, world!</p>-->
+          <h1 class="hero-title mb-4">Your Biblioteca</h1>
+          <p class="hero-subtitle"><span class="typed" data-typed-items="An Online Book Library"></span></p>
+          <!-- <p class="pt-3"><a class="btn btn-primary btn js-scroll px-4" href="#about" role="button">Learn More</a></p> -->
+        </div>
+      </div>
+    </div>
+</div><!-- End Hero Section -->
+
     <!-- ======= Contact Section ======= -->
     <section id="contact" class="paralax-mf footer-paralax bg-image sect-mt4 route" style="background-image: url(assets/img/overlay-bg.jpg)">
       <div class="overlay-mf"></div>

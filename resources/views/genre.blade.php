3200d3cc4bb0fcba6fd8804466b6426b54842b86
hmtori <hnllee90@gmail.com>
2024-06-12 22:22:45 +0800

    upd genre.blade n readerprofile

--- resources/views/genre.blade.php
+++ resources/views/genre.blade.php
@@ -28,6 +28,20 @@
 @extends('master.layout')
 @section('content')
 
+<div id="hero" class="hero route bg-image" style="background-image: url(assets/img/hero-bg.jpg)">
+    <div class="overlay-itro"></div>
+    <div class="hero-content display-table">
+      <div class="table-cell">
+        <div class="container">
+          <!--<p class="display-6 color-d">Hello, world!</p>-->
+          <h1 class="hero-title mb-4">Your Biblioteca</h1>
+          <p class="hero-subtitle"><span class="typed" data-typed-items="Designer, Developer, Freelancer, Photographer"></span></p>
+          <!-- <p class="pt-3"><a class="btn btn-primary btn js-scroll px-4" href="#about" role="button">Learn More</a></p> -->
+        </div>
+      </div>
+    </div>
+</div><!-- End Hero Section -->
+
     <section id="contact" class="paralax-mf footer-paralax bg-image sect-mt4 route" style="background-image: url(assets/img/overlay-bg.jpg)">
       <div class="overlay-mf"></div>
       <div class="container">
